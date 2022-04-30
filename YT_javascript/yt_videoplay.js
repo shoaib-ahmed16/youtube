eb16a181d4c78a49c4b9c58f09799f8071f2b4ef
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-04-30 09:21:00 +0530

    youtube files

---
+++ YT_javascript/yt_videoplay.js
@@ -0,0 +1,53 @@
+function youtube_home_layout()
+{
+    return `<div id="yt_navbar">
+    <div id="firstdiv"><div id="drop_menu"><div id="hr"><img src="menu icone.png"></div></div><img id ="nav_yt_icon" src="icone youtube.png"><span>YouTube</span></div>
+    <div id="seconddiv"><input id="search_in_yt" type="text" placeholder="Search"><div id="sample"></div><img id="mic_search" src="youtubemic.png"></div>
+    <div id="thirdDiv"><div style="width: 40%;"></div><div><img id="rigthImg" src="video adding icon.png"><img id="rigthImg" src="user interface.png"><img id="rigthImg" src="ibell ghanti.png"><img id="rigthImg" style="border-radius: 16px;" src="profile_picture_user_icon_153847.png"></div></div>
+</div>
+<div id="body">
+    <div id="vertical">
+        <div class="left">
+            <div class="sidebar-item"><img><span>Home</span></div>
+            <div class="sidebar-item"><img><span>Explore</span></div>
+            <div class="sidebar-item">Subscriptions</div>
+            <hr>
+            <div class="sidebar-item">Library</div>
+            <div class="sidebar-item">History</div>
+            <div class="sidebar-item">Your Videos</div>
+            <div class="sidebar-item">Watch Later</div>
+            <div class="sidebar-item">Show more</div>
+            <hr>
+            <div class="sidebar-item colorCHnag">SUBSCRIPTIONS</div>
+            <hr>
+            <div class="sidebar-item colorCHnag">MORE FROM YOUTUBE</div>
+            <div class="sidebar-item">YouTube Premium</div>
+            <div class="sidebar-item">Movies</div>
+            <div class="sidebar-item">Gaming</div>
+            <div class="sidebar-item">Live</div>
+            <div class="sidebar-item">Fashion & Beauty</div>
+            <div class="sidebar-item">Learning</div>
+            <hr>
+            <div class="sidebar-item">Settings</div>
+            <div class="sidebar-item">Report History</div>
+            <div class="sidebar-item">Help</div>
+            <div class="sidebar-item">Send FeedBack</div>
+            <hr>
+            <p>
+
+            </p>
+        </div>
+    </div>
+    <div id="vertical_everything">
+        <hr>
+        <div id="vertical_everything_display_search">
+            <div id="main">
+                <div id="video_details"></div>
+                <div id="recommandation"></div>
+            </div>
+        </div>
+    </div>
+</div>`
+}
+
+export default youtube_home_layout;
